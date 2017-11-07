d11541a4197edc7dbcf3dc7fc4aea3eebc0b2f8a
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2017-11-07 13:58:46 +0100

    Exercises 4, 5 and 6 solved

--- functions.php
+++ functions.php
@@ -1,8 +1,30 @@
 <?php
 // "HTTP/1.0 404 Not Found"
-function status_header($code = 200) {}
+function status_header($code = 200) {
+    $message = [
+        200 => "OK",
+        404 => "Page not found",
+        302 => "Moved temporarily",
+        303 => "Something something",
+        500 => "Server error"
+    ];
+    header("HTTP/1.0 ".$code." ".$message[$code]);
+}
+status_header();
 
 // [header => värde]
-function headers(array $headers = []) {}
+function headers(array $headers = []) {
+    foreach ($headers as $header => $value) {
+        header("$header: $value");
+    }
+}
+headers([
+    "Connection" => "keep-alive"
+]);
 
-function redirect($url, $code = 302) {}
+function redirect($url, $code = 302) {
+    status_header($code);
+    header("Location: $url");
+    exit;
+}
+redirect("http://medium.com");
